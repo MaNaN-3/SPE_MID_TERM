1fbb63229d8443c471845cdf9cf0b8cb1d677bb9
MaNaN-3 <Manan.Patel@iiitb.ac.in>
2023-11-29 07:48:43 +0530

    few changes

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -11,12 +11,12 @@ public class Main {
         logger.info("A fundamental calculator for exponential, nCr and Greatest Common Divisor");
         System.out.println("Welcome to the calculator app");
         int choice = 0;
-        while(choice != 19){
+        while(choice != 4){
             System.out.println("Please choose the operation you want to perform");
             System.out.println("1. Power");
             System.out.println("2. nCr");
             System.out.println("3: GCD");
-            System.out.println("19: Exit");
+            System.out.println("4: Exit");
             choice = reader.nextInt();
             if(choice == 1) {
                 System.out.println("You have chosen power function");
